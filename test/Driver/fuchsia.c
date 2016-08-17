d8a3ac6da343b969211b634dd52882ba748c3330
Petr Hosek <phosek@google.com>
2016-08-17 12:21:24 -0700

    Update Clang driver tests for Fuchsia
    The driver tests should cover dynamically linked executables which
    are now default.
    
    Change-Id: I920a0767c98a61cc5e3be50d8c4e2698fbd37471

--- test/Driver/fuchsia.c
+++ test/Driver/fuchsia.c
@@ -6,16 +6,27 @@
 // CHECK: "-internal-externc-isystem" "[[SYSROOT]]{{/|\\\\}}include"
 // CHECK: {{.*}}lld{{.*}}" "-flavor" "gnu"
 // CHECK: "--sysroot=[[SYSROOT]]"
-// CHECK: "-Ttext-segment=0x1000000"
-// CHECK: crt1.o
+// CHECK: "-pie"
+// CHECK: "-dynamic-linker" "ld.so.1"
+// CHECK: Scrt1.o
 // CHECK-NOT: crti.o
 // CHECK-NOT: crtbegin.o
 // CHECK: "-L[[SYSROOT]]/lib"
 // CHECK: "{{.*[/\\]}}libclang_rt.builtins-x86_64.a"
-// CHECK: "-lc"
+// CHECK: "-Bdynamic" "-lc"
 // CHECK-NOT: crtend.o
 // CHECK-NOT: crtn.o
 
 // RUN: %clang %s -### --target=x86_64-unknown-fuchsia -rtlib=libgcc 2>&1 \
 // RUN:     | FileCheck %s -check-prefix=CHECK-RTLIB
 // CHECK-RTLIB: error: invalid runtime library name in argument '-rtlib=libgcc'
+
+// RUN: %clang %s -### --target=x86_64-unknown-fuchsia -static 2>&1 \
+// RUN:     | FileCheck %s -check-prefix=CHECK-STATIC
+// CHECK-STATIC: "-Bstatic"
+// CHECK-STATIC: "-Bdynamic" "-lc"
+
+// RUN: %clang %s -### --target=x86_64-unknown-fuchsia -shared 2>&1 \
+// RUN:     | FileCheck %s -check-prefix=CHECK-SHARED
+// CHECK-SHARED-NOT: "-pie"
+// CHECK-SHARED: "-shared"
